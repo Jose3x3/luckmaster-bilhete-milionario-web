4922a2f4c2bf01f10c8487d2a9f499ae59ec3bd9
Ronaldo Gomes <ronaldosg1999@outlook.com>
2024-07-29 13:32:07 -0300

    fix: responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image'
-import BilheteMilionarioLogo from '@/assets/imgs/bilhete_milionario.png'
+import Banner from '@/assets/imgs/banner.jpg'
 
 export default function Home() {
   return (
-    <main className="bg-primary-900 text-white flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <h1 className="font-semibold text-4xl">Em breve mais informações.</h1>
+    <main className="bg-primary-900 text-white flex min-h-screen flex-col items-center justify-center gap-4 md:p-24">
+      <h1 className="font-semibold text-center sm:text-2xl md:text-4xl">
+        Em breve, no dia 02/08, a ação terá início.
+      </h1>
       <Image
         width={500}
         height={500}
-        src={BilheteMilionarioLogo}
-        alt="Bilhete Milionário Logo"
+        src={Banner}
+        alt="Bilhete Milionário Banner"
       />
     </main>
   )
