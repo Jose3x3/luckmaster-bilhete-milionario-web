ea94eb101d5cfa1f52451d4cdf97f3d5fa1d49b6
Ronaldo Gomes <ronaldosg1999@outlook.com>
2024-07-29 13:32:07 -0300

    fix: responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import BilheteMilionarioLogo from '@/assets/imgs/bilhete_milionario.png'
 
 export default function Home() {
   return (
-    <main className="bg-primary-900 text-white flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <h1 className="font-semibold text-4xl">Em breve mais informações.</h1>
+    <main className="bg-primary-900 text-white flex min-h-screen flex-col items-center justify-center gap-4 md:p-24">
+      <h1 className="font-semibold text-center sm:text-2xl md:text-4xl">
+        Em breve mais informações.
+      </h1>
       <Image
         width={500}
         height={500}
